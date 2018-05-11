eb2e181ca339bd8aa37691f959b0ea7372a0420a
Your Name <u0652010@nkfust.edu.tw>
2018-05-11 10:40:57 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b=10,s=0;
+
+for(a=1;a<=b;a++)
+{
+
+	s =s+ a;
+	printf("nini= %d\n",s);
+}
+
+	
+	return 0;
+}
+
+
+
+
+
